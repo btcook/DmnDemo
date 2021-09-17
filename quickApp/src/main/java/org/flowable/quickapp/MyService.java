c4f488f5dd527b9083345bb8e6002276c4107bd7
Ben Cook <50178137+btcook@users.noreply.github.com>
2021-09-17 06:05:47 -0700

    Add files via upload

---
+++ quickApp/src/main/java/org/flowable/quickapp/MyService.java
@@ -0,0 +1,48 @@
+package org.flowable.quickapp;
+
+
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.task.api.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Transactional//could be using the wrong type, look under JPA section
+public class MyService {
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public void startProcess(String assignee) {
+
+        Person person = personRepository.findByUsername(assignee);
+
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("person", person);
+        runtimeService.startProcessInstanceByKey("oneTaskProcess", variables);
+    }
+
+    public List<Task> getTasks(String assignee) {
+        return taskService.createTaskQuery().taskAssignee(assignee).list();
+    }
+
+    public void createDemoUsers() {
+        if (personRepository.findAll().size() == 0) {
+            personRepository.save(new Person("jbarrez", "Joram", "Barrez", new Date()));
+            personRepository.save(new Person("trademakers", "Tijs", "Rademakers", new Date()));
+        }
+    }
+
+}
---
+++ quickApp/src/main/java/org/flowable/quickapp/QuickAppApplication.java
@@ -0,0 +1,33 @@
+package org.flowable.quickapp;
+
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.task.api.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class QuickAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickAppApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner init(final MyService myService) {
+
+        return new CommandLineRunner() {
+            public void run(String... strings) throws Exception {
+                myService.createDemoUsers();
+            }
+        };
+    }
+}
